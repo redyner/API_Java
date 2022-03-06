147f2ad78f3d3e5efbe189edec69c9397b27fb5b
Rediner <rediner@hotmail.com>
2022-03-06 20:54:21 -0300

    Criação de classe de teste

--- src/API/Clientes.java
+++ src/API/Clientes.java
@@ -4,8 +4,10 @@ public class Clientes {
 
 	public static void main(String[] args) 
 	{
-		Thread t1 = new Cliente();
-		Thread t2 = new Cliente();
+		//Thread t1 = new Cliente();
+		//Thread t2 = new Cliente();
+		Thread t1 = new Teste("Primeira");
+		Thread t2 = new Teste("Segunda");
 	}
 
 }
